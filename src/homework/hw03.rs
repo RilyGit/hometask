748f1d12b314ce61e6f205f2cf820a042274a519
Rily <Ruslanius.fan@gmail.com>
2025-04-13 12:22:38 +0300

    A critical error has been fixed

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,21 +1,29 @@
-fn char_at(x: usize, y: usize, width: usize, height: usize) -> char {
-    if y == 0 || y == height - 1 || x == 0 || x == width - 1 || x == y || x == width - 1 - y {
+fn charpix(x: usize, y: usize, width: usize, height: usize) -> char {
+    let border = x == 0 || x == width - 1 || y == 0 || y == height - 1;
+  
+    let lposition = (y * (width - 1)) / (height - 1);
+    let rposition = ((height - 1 - y) * (width - 1)) / (height - 1);
+
+    let diagonal = x == lposition || x == rposition;
+    
+    if border || diagonal {
         '*'
-    } else {    // (: 
+    } else {
         ' '
     }
 }
+
 fn main() {
-    const WIDTH: usize = 50;
-const HEIGHT: usize = 50;
-
-let mut output = String::new();
-
-for y in 0..HEIGHT {
-for x in 0..WIDTH {
- output.push(char_at(x, y, WIDTH, HEIGHT));
- }
-output.push('\n');
- }
- print!("{}", output);
+    const WIDTH: usize = 28;
+    const HEIGHT: usize = 10;
+    
+    let mut output = String::new();
+    
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            output.push(charpix(x, y, WIDTH, HEIGHT));
+        }
+        output.push('\n');
+    }
+    print!("{}", output);
 }
